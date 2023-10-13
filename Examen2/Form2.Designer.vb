5678eae074037622b8e5f2109bd2f3a8d90dc31c
Jose Argueta <alejandroarguetaleal@hotmail.com>
2023-10-13 10:49:12 -0600

    Operaciones 2.0

--- Examen2/Form2.Designer.vb
+++ Examen2/Form2.Designer.vb
@@ -22,15 +22,83 @@ Partial Class Form2
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Button4 = New Button()
+        Button3 = New Button()
+        Button2 = New Button()
+        Button1 = New Button()
+        Label2 = New Label()
         SuspendLayout()
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.Cyan
+        Button4.Location = New Point(289, 248)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(112, 34)
+        Button4.TabIndex = 11
+        Button4.Text = "DIVICION"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Coral
+        Button3.Location = New Point(261, 208)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(158, 34)
+        Button3.TabIndex = 10
+        Button3.Text = "MULTIPLICACION"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Fuchsia
+        Button2.Location = New Point(289, 168)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 9
+        Button2.Text = "RESTA"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.CornflowerBlue
+        Button1.ForeColor = SystemColors.ActiveCaptionText
+        Button1.Location = New Point(289, 128)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 8
+        Button1.Text = "SUMA"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(196, 68)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(334, 41)
+        Label2.TabIndex = 7
+        Label2.Text = "OPERACIONES BASICAS"
         ' 
         ' Form2
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
         Name = "Form2"
         Text = "Form2"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
 End Class
