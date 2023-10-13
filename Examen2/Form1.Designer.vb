d7f738ceb02c05074fab8d93c02b440680b0a5e7
Jose Argueta <alejandroarguetaleal@hotmail.com>
2023-10-13 10:32:10 -0600

    LOGIN 1.0

--- Examen2/Form1.Designer.vb
+++ Examen2/Form1.Designer.vb
@@ -24,6 +24,11 @@ Partial Class Form1
     Private Sub InitializeComponent()
         LinkLabel1 = New LinkLabel()
         Label1 = New Label()
+        Label2 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
         SuspendLayout()
         ' 
         ' LinkLabel1
@@ -42,11 +47,68 @@ Partial Class Form1
         Label1.Size = New Size(0, 25)
         Label1.TabIndex = 1
         ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(308, 35)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(105, 41)
+        Label2.TabIndex = 2
+        Label2.Text = "LOGIN"
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.CornflowerBlue
+        Button1.ForeColor = SystemColors.ActiveCaptionText
+        Button1.Location = New Point(308, 92)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 3
+        Button1.Text = "SUMA"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.Fuchsia
+        Button2.Location = New Point(308, 132)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 4
+        Button2.Text = "RESTA"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.Coral
+        Button3.Location = New Point(280, 172)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(158, 34)
+        Button3.TabIndex = 5
+        Button3.Text = "MULTIPLICACION"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.Cyan
+        Button4.Location = New Point(308, 212)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(112, 34)
+        Button4.TabIndex = 6
+        Button4.Text = "DIVICION"
+        Button4.UseVisualStyleBackColor = False
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.ControlDark
         ClientSize = New Size(800, 450)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label2)
         Controls.Add(Label1)
         Controls.Add(LinkLabel1)
         Name = "Form1"
@@ -57,4 +119,9 @@ Partial Class Form1
 
     Friend WithEvents LinkLabel1 As LinkLabel
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
 End Class
